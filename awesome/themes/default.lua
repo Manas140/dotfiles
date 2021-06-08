8ca3a8521374098161c902526a050df14c6e7afb
Manas <64472354+Manas140@users.noreply.github.com>
2021-06-08 13:09:13 +0530

    Add files via upload

---
+++ awesome/themes/default.lua
@@ -0,0 +1,30 @@
+-- Default
+------------------
+
+local theme = {}
+
+theme.font          = 'Iosevka Nerd Font 10'
+
+
+theme.useless_gap   = 3
+theme.border_width  = 0 
+theme.border_normal = "#202020"
+theme.border_focus  = "#ff9999"
+theme.border_marked = "#ff7777"
+
+-- Hotkeys popup
+theme.hotkeys_bg = '#202020'
+theme.hotkeys_fg = '#ffffce'
+theme.hotkeys_opacity = '0'
+theme.hotkeys_border_width = 3
+theme.hotkeys_border_color = '#ff9999'
+theme.hotkeys_modifiers_fg = '#ff7777'
+theme.hotkeys_label_fg = '#202020'
+theme.hotkeys_font = 'Iosevka Nerd Font Bold 10'
+theme.hotkeys_description_font = 'Noto Font 10'
+theme.hotkeys_group_margin = 5
+
+-- Wallpaper
+theme.wallpaper = '$HOME/.config/awesome/themes/wallpaper.png'
+
+return theme
