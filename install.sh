b5f96702271f1c002735e4ba20423691dd825a98
manas140 <manasbhosale.14@gmail.com>
2021-06-30 13:00:14 +0000

    try

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 
-echo -e [*] This Config Replaces Your Previous Config Make A Backup Before Running ./install.sh -y
+echo -e [*] This Config Replaces Your Previous Config, Make A Backup Before Running ./install.sh -y
 
 if [[ "$1" == "-y" ]]; then
     cp -r fonts/* ~/.fonts/
     echo -e [*] Fonts Installed
-    cp -r * ~/.config/
-    rm ~/.config/README.md ~/.config/install.sh
+    cp -r walls/* ~/Pictures/Wallpapers/.
+    echo -e [*] Wallpapers Installed
+    cp -r * ~/.config/.
+    rm -r ~/.config/README.md ~/.config/install.sh ~/.config/fonts/ ~/.config/walls/
     echo -e [*] Config Installed
 fi
 
