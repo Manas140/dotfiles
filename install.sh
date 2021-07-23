397336006aaca786309e7ae23a291dd06f56db91
manas140 <manasbhosale.14@gmail.com>
2021-07-23 16:16:54 +0000

    installer colors

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
+#variables
+green="\033[1;32m"
+blue="\033[1;34m"
+red="\033[1;31m"
+magenta="\033[1;35m"
+end="\033[1;0m"
 
-echo -e [*] This Config Replaces Your Previous Config, Make A Backup Before Running It.
+echo -e "${green}[*] This Config Replaces Your Previous Config, Make A Backup Before Running It.${end}"
 
-echo "[*] Proceeding Here Will Replace Your Config(If It's There)."
-read -p "[*] DO YOU WANT TO PROCEED [Y/N] " allowed
+echo -e "${blue} [*] Proceeding Here Will Replace Your Config(If It's There).${end}"
+read -p "  [*] DO YOU WANT TO PROCEED [Y/N] " allowed
 if [[ $allowed = Y* || $allowed = y* ]]; then
     cp -r fonts/* ~/.fonts/
-    echo -e [*] Fonts Installed
+    echo -e "${magenta}   [*] Fonts Installed"
     cp -r walls/* ~/Pictures/Wallpapers/.
-    echo -e [*] Wallpapers Installed
+    echo -e "${magenta}   [*] Wallpapers Installed${end}"
     cp -r * ~/.config/.
     rm -r ~/.config/README.md ~/.config/install.sh ~/.config/fonts/ ~/.config/walls/
-    echo -e [*] Config Installed
+    echo -e "${green}[*] Config Installed${end}"
 else
-    echo -e Aborting!
+    echo -e "${red}[-] Aborting!${end}"
 fi
