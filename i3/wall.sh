caaab0652ff0a3ada22aa13c9272e633f23c1354
manas140 <manasbhosale.14@gmail.com>
2021-07-23 15:41:04 +0000

    better color and better wall.sh script

--- i3/wall.sh
+++ i3/wall.sh
@@ -1,1 +1,7 @@
-feh --bg-fill ~/Pictures/Wallpapers/bg_$(($RANDOM%6)).jpg
+#!/usr/bin/env bash
+walls_dir=~/Pictures/Wallpapers
+walls=($(echo $walls_dir/*))
+number_of_walls=$(ls $walls_dir | wc -l )
+random_number=$(($RANDOM%$number_of_walls))
+wall=${walls[$(echo $random_number)]}
+feh --bg-fill $wall
